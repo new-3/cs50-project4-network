9c06272c6c5700b3d095b2a0eb63e62a86a9f31f
= <=>
2024-11-30 21:39:33 +0900

    Make first models, reset db

--- network/models.py
+++ network/models.py
@@ -1,12 +1,58 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
-    likes = models.ManyToManyField('Post', related_name="liked_by")
+
+    def __str__(self) -> str:
+        return f"{self.username}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="network/users/pictures/", default="network/users/pictures/default.jpg")
+    following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    follower_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
+
+
+# Update followers/following count when related change occured.
+@receiver(post_save, sender=UserProfile.following.through)
+def update_follower_count_on_add(sender, instance, **kwargs):
+    instance.follower_count = instance.followers.count()
+    instance.save()
+
+@receiver(post_delete, sender=UserProfile.following.through)
+def update_follower_count_on_remove(sender, instance, **kwargs):
+    instance.follower_count = instance.followers.count()
+    instance.save()
+
+@receiver(post_save, sender=UserProfile.followers.through)
+def update_following_count_on_add(sender, instance, **kwargs):
+    instance.following_count = instance.following.count()
+    instance.save()
+
+@receiver(post_delete, sender=UserProfile.followers.through)
+def update_following_count_on_remove(sender, instance, **kwargs):
+    instance.following_count = instance.following.count()
+    instance.save()
 
 
 class Post(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='posts')
+    body = models.TextField(null=False, default="EMPTY")
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField('UserProfile', related_name="likes")
+    like_count = models.IntegerField(default=0)
 
+@receiver(post_save, sender=Post.likes.through)
+def update_likes_count_on_add(sender, instance, **kwargs):
+    instance.like_count = instance.likes.count()
+    instance.save()
 
+@receiver(post_delete, sender=Post.likes.through)
+def update_likes_count_on_remove(sender, instance, **kwargs):
+    instance.like_count = instance.likes.count()
+    instance.save()
