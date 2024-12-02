c5c2ae3d4b993cf6ce18e69aaad4314799216e0c
new-3 <28271599+new-3@users.noreply.github.com>
2024-12-02 06:44:26 +0000

    Change to PostgreSQL

--- project4/settings.py
+++ project4/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'securepassword',
+        'HOST': 'postgres_container',
+        'PORT': '5432',
     }
 }
 
